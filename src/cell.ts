3fd5aab845448c73ec80c1d8d296aa97ed91378e
Eugene-Tshien <100738608+Eugene-Tshien@users.noreply.github.com>
2022-08-21 02:30:40 +1000

    center and buttons(kinda)

--- src/cell.ts
+++ src/cell.ts
@@ -1,14 +1,22 @@
 const table = document.getElementById("table") as HTMLElement; 
 
-const width = 9;
+const width = 3;
 
 for(let i = 0; i <= width; i++)
 {
     for(let j = 0; j <= width; j++)
     {
-        table.innerHTML += `<div class=\"item\" id = \"c${i}${j}\">${i}${j}</div>`;
+        if(i <= width - 1 && j <= width - 1){
+            table.innerHTML += `<div class=\"item\" id = \"c${i}${j}\">${i}${j}<div class=\"button\" id=\"b${i}${j}\"></div></div>`;
+        }else{
+            table.innerHTML += `<div class=\"item\" id = \"c${i}${j}\">${i}${j}</div>`;
+        }
+        
     }
 
     table.style.gridTemplateColumns += " auto";
 }
+
+table.style.left = `${(document.body.clientWidth/2) - (table.clientWidth/2)}` + 'px';
+
 
