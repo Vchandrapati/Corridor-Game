be0f8372902069a2c90f0591bddecf86e5da11dc
Eugene-Tshien <100738608+Eugene-Tshien@users.noreply.github.com>
2022-08-21 20:00:59 +1000

    8pm

--- dist/button.js
+++ dist/button.js
@@ -5,13 +5,7 @@ for (let i = 0; i < 8; i++) {
     for (let j = 0; j < 8; j++) {
         myButtons[count] = document.getElementById(`b${i}${j}`);
         myButtons[count].addEventListener("click", () => {
-            if (document.getElementById("c00").style.borderBottomColor != "red") {
-                document.getElementById("c00").style.borderBottomColor = "red";
-                document.getElementById("c01").style.borderBottomColor = "red";
-                document.getElementById("c10").style.borderTopColor = "red";
-                document.getElementById("c11").style.borderTopColor = "red";
-                window.alert(`${i}${j}`);
-            }
+            window.alert(`${i}${j}`);
         });
         count++;
     }
