35b54a09783cecaf190e7881c4296eef26047a4a
Vikil Chandrapati <79786184+Vchandrapati@users.noreply.github.com>
2022-08-22 17:34:57 +1000

    updated

--- dist/button.js
+++ dist/button.js
@@ -5,13 +5,6 @@ let rotate = false, mOut = false;
 let count = 0;
 for (let i = 0; i < 8; i++) {
     for (let j = 0; j < 8; j++) {
-        check();
-        function check() {
-            if (skipArr.includes(`${i}${j}`)) {
-                console.log("hi");
-                count++;
-            }
-        }
         myButtons[count] = document.getElementById(`b${i}${j}`);
         myButtons[count].addEventListener("contextmenu", () => {
             console.log("hi");
