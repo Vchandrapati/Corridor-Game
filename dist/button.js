ae769eec9983ec73d8a246366abf7a423d153d18
Eugene-Tshien <100738608+Eugene-Tshien@users.noreply.github.com>
2022-08-22 17:30:19 +1000

    correct

--- dist/button.js
+++ dist/button.js
@@ -5,15 +5,7 @@ let rotate = false, mOut = false;
 let count = 0;
 for (let i = 0; i < 8; i++) {
     for (let j = 0; j < 8; j++) {
-        check();
-        function check() {
-            if (skipArr.includes(`${i}${j}`)) {
-                console.log("hi");
-                count++;
-            }
-        }
         myButtons[count] = document.getElementById(`b${i}${j}`);
-
         myButtons[count].addEventListener("contextmenu", () => {
             console.log("hi");
             if (rotate)
@@ -22,6 +14,12 @@ for (let i = 0; i < 8; i++) {
                 rotate = true;
             clear();
             preview("red");
+        });
+        myButtons[count].addEventListener('dblclick', () => {
+            console.log(`double clicked b${i}${j}`);
+        });
+        myButtons[count].addEventListener("click", () => {
+            console.log(`clicked b${i}${j}`);
         });
         myButtons[count].addEventListener("click", () => {
             console.log("hi");
@@ -44,21 +42,12 @@ for (let i = 0; i < 8; i++) {
         });
         myButtons[count].addEventListener("mouseover", () => {
             preview("red");
-
-        myButtons[count].addEventListener('dblclick', () => {
-            console.log(`double clicked b${i}${j}`);
-        });
-        myButtons[count].addEventListener("click", () => {
-            console.log(`clicked b${i}${j}`);
-        });
-        myButtons[count].addEventListener("mouseover", () => {
-            console.log(`hovered b${i}${j}`);
-
         });
         myButtons[count].addEventListener("mouseout", () => {
             if (!mOut)
                 preview("black");
             mOut = false;
+            console.log(`hovered b${i}${j}`);
         });
         function preview(colour) {
             if (!rotate) {
--- src/button.ts
+++ src/button.ts
@@ -4,15 +4,6 @@ let rotate = false, mOut = false;
 let count = 0;
 for(let i = 0; i < 8; i++){
     for(let j = 0; j < 8; j++){
-        check();
-        function check() {
-            if(skipArr.includes(`${i}${j}`))
-            {
-                console.log("hi");
-                count++;
-            }
-        }
-
         myButtons[count] = document.getElementById(`b${i}${j}`) as HTMLElement; 
 
         myButtons[count].addEventListener("contextmenu", () => {
@@ -25,14 +16,8 @@ for(let i = 0; i < 8; i++){
             
             clear();
             preview("red");
-
-        myButtons[count].addEventListener('dblclick', () => {
-            console.log(`double clicked b${i}${j}`);
         });
-
-        myButtons[count].addEventListener("click", () => {
-            console.log(`clicked b${i}${j}`);
-        });
+        
         myButtons[count].addEventListener("click", () => {
             console.log("hi");
             document.getElementById(`b${i}${j}`)!.style.display = "none";
