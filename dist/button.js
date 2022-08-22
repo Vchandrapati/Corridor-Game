61c017025b8a0a2c6ebfa03d240f99a7c0dfecc3
Kyluuuu <thomas.mikic2@students.mq.edu.au>
2022-08-22 19:20:12 +1000

    invalid code - thomas

--- dist/button.js
+++ dist/button.js
@@ -1,25 +1,27 @@
 "use strict";
 let myButtons = [];
 let skipArr = [];
-let rotate = false, mOut = false;
+let rotate = false, mOut = false, vertOnly = false, horzOnly = false;
 let count = 0;
 for (let i = 0; i < 8; i++) {
     for (let j = 0; j < 8; j++) {
         myButtons[count] = document.getElementById(`b${i}${j}`);
+        //add listener for right click for the buttons
         myButtons[count].addEventListener("contextmenu", () => {
-            console.log("hi");
-            if (rotate)
+            if (rotate) {
                 rotate = false;
-            else
+            } //
+            else {
                 rotate = true;
+            }
             clear();
             preview("red");
         });
+        //add listener for left click for the buttons
         myButtons[count].addEventListener("click", () => {
-            console.log("hi");
             document.getElementById(`b${i}${j}`).style.display = "none";
             mOut = true;
-            let color = "rgba(0, 99, 71, 0)";
+            let color = "rgba(0, 99, 71, 0.5)";
             if (rotate) {
                 console.log(rotate);
                 document.getElementById(`c${i}${j}`).style.borderRightColor = "color";
@@ -46,27 +48,44 @@ for (let i = 0; i < 8; i++) {
         });
         function preview(colour) {
             if (!rotate) {
-                document.getElementById(`c${i}${j}`).style.borderBottomColor = `${colour}`;
-                document.getElementById(`c${i + 1}${j}`).style.borderTopColor = `${colour}`;
-                document.getElementById(`c${i}${j + 1}`).style.borderBottomColor = `${colour}`;
-                document.getElementById(`c${i + 1}${j + 1}`).style.borderTopColor = `${colour}`;
+                if (!vertOnly) {
+                    document.getElementById(`c${i}${j}`).style.borderBottomColor = `${colour}`;
+                    document.getElementById(`c${i + 1}${j}`).style.borderTopColor = `${colour}`;
+                    document.getElementById(`c${i}${j + 1}`).style.borderBottomColor = `${colour}`;
+                    document.getElementById(`c${i + 1}${j + 1}`).style.borderTopColor = `${colour}`;
+                }
             }
             else {
-                document.getElementById(`c${i}${j}`).style.borderRightColor = `${colour}`;
-                document.getElementById(`c${i + 1}${j}`).style.borderRightColor = `${colour}`;
-                document.getElementById(`c${i}${j + 1}`).style.borderLeftColor = `${colour}`;
-                document.getElementById(`c${i + 1}${j + 1}`).style.borderLeftColor = `${colour}`;
+                if (!horzOnly) {
+                    document.getElementById(`c${i}${j}`).style.borderRightColor = `${colour}`;
+                    document.getElementById(`c${i + 1}${j}`).style.borderRightColor = `${colour}`;
+                    document.getElementById(`c${i}${j + 1}`).style.borderLeftColor = `${colour}`;
+                    document.getElementById(`c${i + 1}${j + 1}`).style.borderLeftColor = `${colour}`;
+                }
             }
         }
         function clear() {
-            document.getElementById(`c${i}${j}`).style.borderBottomColor = "black";
+            document.getElementById(`c${i}${j}`).style.borderBottomColor = "black"; //left
             document.getElementById(`c${i + 1}${j}`).style.borderTopColor = "black";
-            document.getElementById(`c${i}${j + 1}`).style.borderBottomColor = "black";
+            document.getElementById(`c${i}${j + 1}`).style.borderBottomColor = "black"; //right
             document.getElementById(`c${i + 1}${j + 1}`).style.borderTopColor = "black";
-            document.getElementById(`c${i}${j}`).style.borderRightColor = "black";
+            document.getElementById(`c${i}${j}`).style.borderRightColor = "black"; //top
             document.getElementById(`c${i + 1}${j}`).style.borderRightColor = "black";
-            document.getElementById(`c${i}${j + 1}`).style.borderLeftColor = "black";
+            document.getElementById(`c${i}${j + 1}`).style.borderLeftColor = "black"; //bottom
             document.getElementById(`c${i + 1}${j + 1}`).style.borderLeftColor = "black";
+        }
+        //checks whether a block can be placed by seeing if the adjacent blocks are red or not
+        function invalid() {
+            if (rotate) {
+                if (document.getElementById(`c${i}${j}`).style.borderRightColor == "red" || document.getElementById(`c${i}${j + 1}`).style.borderLeftColor == "red") {
+                    horzOnly = true;
+                }
+            }
+            else {
+                if (document.getElementById(`c${i}${j}`).style.borderBottomColor == "red" || document.getElementById(`c${i}${j + 1}`).style.borderBottomColor == "red") {
+                    vertOnly = true;
+                }
+            }
         }
         count++;
     }
--- src/button.ts
+++ src/button.ts
@@ -1,23 +1,20 @@
-let myButtons = [];
-let skipArr: string[] = [];
-let rotate = false, mOut = false;
+let myButtons = []
+let skipArr: string[] = []
+let rotate = false, mOut = false, vertOnly = false, horzOnly = false
 let count = 0;
 for(let i = 0; i < 8; i++){
     for(let j = 0; j < 8; j++){
         myButtons[count] = document.getElementById(`b${i}${j}`) as HTMLElement; 
 
+        //add listener for right click for the buttons
         myButtons[count].addEventListener("contextmenu", () => {
-            console.log("hi");
-            if(rotate)
-            rotate = false;
-            else
-            rotate = true;
-            
-            clear();
-            preview("red");
+            if(rotate) {rotate = false} //
+            else {rotate = true}
+            clear()
+            preview("red")
         });
+        //add listener for left click for the buttons
         myButtons[count].addEventListener("click", () => {
-            console.log("hi");
             document.getElementById(`b${i}${j}`)!.style.display = "none";
             mOut = true;
             let color = "rgba(0, 99, 71, 0.5)";
@@ -54,28 +51,47 @@ for(let i = 0; i < 8; i++){
         function preview(colour:String)
         {           
             if(!rotate) {
-                document.getElementById(`c${i}${j}`)!.style.borderBottomColor = `${colour}`;
-                document.getElementById(`c${i + 1}${j}`)!.style.borderTopColor = `${colour}`;
-                document.getElementById(`c${i}${j + 1}`)!.style.borderBottomColor = `${colour}`;
-                document.getElementById(`c${i + 1}${j + 1}`)!.style.borderTopColor = `${colour}`;
+                if(!vertOnly) {
+                    document.getElementById(`c${i}${j}`)!.style.borderBottomColor = `${colour}`;
+                    document.getElementById(`c${i + 1}${j}`)!.style.borderTopColor = `${colour}`;
+                    document.getElementById(`c${i}${j + 1}`)!.style.borderBottomColor = `${colour}`;
+                    document.getElementById(`c${i + 1}${j + 1}`)!.style.borderTopColor = `${colour}`;
+                }
             } else {
-                document.getElementById(`c${i}${j}`)!.style.borderRightColor = `${colour}`;
-                document.getElementById(`c${i + 1}${j}`)!.style.borderRightColor = `${colour}`;
-                document.getElementById(`c${i}${j + 1}`)!.style.borderLeftColor = `${colour}`;
-                document.getElementById(`c${i + 1}${j + 1}`)!.style.borderLeftColor = `${colour}`;
+                if(!horzOnly) {
+                    document.getElementById(`c${i}${j}`)!.style.borderRightColor = `${colour}`;
+                    document.getElementById(`c${i + 1}${j}`)!.style.borderRightColor = `${colour}`;
+                    document.getElementById(`c${i}${j + 1}`)!.style.borderLeftColor = `${colour}`;
+                    document.getElementById(`c${i + 1}${j + 1}`)!.style.borderLeftColor = `${colour}`;
+                }
             }
         }
 
         function clear()
         {
-            document.getElementById(`c${i}${j}`)!.style.borderBottomColor = "black";
+            document.getElementById(`c${i}${j}`)!.style.borderBottomColor = "black";      //left
             document.getElementById(`c${i + 1}${j}`)!.style.borderTopColor = "black";
-            document.getElementById(`c${i}${j + 1}`)!.style.borderBottomColor = "black";
+            document.getElementById(`c${i}${j + 1}`)!.style.borderBottomColor = "black";  //right
             document.getElementById(`c${i + 1}${j + 1}`)!.style.borderTopColor = "black";
-            document.getElementById(`c${i}${j}`)!.style.borderRightColor = "black";
+            document.getElementById(`c${i}${j}`)!.style.borderRightColor = "black";       //top
             document.getElementById(`c${i + 1}${j}`)!.style.borderRightColor = "black";
-            document.getElementById(`c${i}${j + 1}`)!.style.borderLeftColor = "black";
+            document.getElementById(`c${i}${j + 1}`)!.style.borderLeftColor = "black";    //bottom
             document.getElementById(`c${i + 1}${j + 1}`)!.style.borderLeftColor = "black";    
+        }
+
+        //checks whether a block can be placed by seeing if the adjacent blocks are red or not
+        function invalid()
+        {
+            if(rotate) {
+                if(document.getElementById(`c${i}${j}`)!.style.borderRightColor == "red" || document.getElementById(`c${i}${j + 1}`)!.style.borderLeftColor == "red") {
+                    horzOnly = true
+                }
+            }
+            else {
+                if(document.getElementById(`c${i}${j}`)!.style.borderBottomColor == "red" || document.getElementById(`c${i}${j + 1}`)!.style.borderBottomColor == "red") {
+                    vertOnly = true
+                }
+            }
         }
 
         count++;
